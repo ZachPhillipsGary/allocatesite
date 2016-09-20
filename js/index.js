c019819ed14aae78fb6d0fb8afb87f9247516097
ZPG <zach.phillips-gary@worldviewsolutions.com>
2016-09-19 17:47:25 -0700

    fixed animation

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ TweenMax.from("#device", 0.8, {y: 50, opacity: 0, ease:Back.easeOut, delay: 1});
 TweenMax.from(".header", 0.8, {opacity: 0, x: 30, delay: 0.6});
 TweenMax.from(".preheader", 1, {opacity: 0, x: -30, delay: 0.2});
 TweenMax.from(".paragraph", 1, {opacity: 0, delay: 1, y: 20});
-TweenMax.staggerFrom(".btn", 0.5, {y: 10, opacity: 0, delay: 1.6}, 0.4);  
+TweenMax.staggerFrom(".signup", 0.5, {y: 10, opacity: 0, delay: 1.6}, 0.4);  
 
 var goalDay = '2017/08/27 00:00:00'
 
